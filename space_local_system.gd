127a6a5f4c9f7732b000d7d7a6490110e7c5bb12
joris <jorisvddonk@gmail.com>
2023-08-01 16:54:21 +0200

    fix planet rotation so that they face the star properly!

--- space_local_system.gd
+++ space_local_system.gd
@@ -5,10 +5,10 @@ var Planet = preload("res://Planet.tscn")
 
 func _ready():
 	regex.compile("\\s*S[0-9][0-9]")
-	Globals.feltyrion.found_planet.connect(_on_found_planet)
+	#Globals.feltyrion.found_planet.connect(_on_found_planet) # temporarily disabled
 	Globals.on_parsis_changed.connect(_on_parsis_changed)
 	
-func _on_found_planet(index, planet_id, seedval, x, y, z, type, owner, moonid, ring, tilt, ray, orb_ray, orb_tilt, orb_orient, orb_ecc, rtperiod, rotation, term_start, term_end, qsortindex, qsortdist):
+func _on_found_planet(index, planet_id, seedval, x, y, z, type, owner, moonid, ring, tilt, ray, orb_ray, orb_tilt, orb_orient, orb_ecc, rtperiod, rotation, viewpoint, term_start, term_end, qsortindex, qsortdist):
 	var planet_name = Globals.feltyrion.get_planet_name_by_id(planet_id)
 	#var planet_name = "planet_name"
 	#printt("Found planet: ", planet_name, index, planet_id, seedval, x, y, z, "----", type, owner, moonid, ring, tilt, ray, orb_ray, orb_tilt, orb_orient, orb_ecc, rtperiod, "rotation", rotation, term_start, term_end, qsortindex, qsortdist)
@@ -19,7 +19,7 @@ func _on_found_planet(index, planet_id, seedval, x, y, z, type, owner, moonid, r
 	planet.planet_name = planet_name
 	#planet.translate(Vector3(index * 5 if owner == -1 else owner * 5, 0 if owner == -1 else (moonid + 1) * -5, 0)) # alternative placement; makes it easy to see all planets in an overview
 	planet.translate(Vector3(-(Globals.feltyrion.ap_target.x - x), (Globals.feltyrion.ap_target.y - y), (Globals.feltyrion.ap_target.z - z))) # TODO: check if y should be flipped here..
-	planet.get_node("PlanetParent").rotate_y(((rotation + 98) % 360) * (PI / 180)) # not entirely sure why '98' is the magic number here (it should be 89..?) but it seems to work!
+	planet.get_node("PlanetParent").rotate_y(((viewpoint + rotation + 89 + 35) % 360) * (PI / 180)) # not entirely sure why '98' is the magic number here (it should be 89..?) but it seems to work!
 	var layer_fixer = func(item): item.set_layer_mask(4); return true
 	planet.find_children("?*", "CSGSphere3D", true).all(layer_fixer)
 	$Planets.add_child(planet)
@@ -32,8 +32,10 @@ func _on_parsis_changed(vec3):
 	print(Time.get_unix_time_from_system())
 	Globals.feltyrion.prepare_star()
 	var data = Globals.feltyrion.get_current_star_info()
+	print(data)
 	for i in range(0,data.nearstar_nob):
 		var pl_data = Globals.feltyrion.get_planet_info(i)
+		print("Found planet: ", pl_data)
 		_on_found_planet(i, pl_data.nearstar_p_identity, pl_data.nearstar_p_seedval,
 			pl_data.nearstar_p_plx,
 			pl_data.nearstar_p_ply,
@@ -50,6 +52,7 @@ func _on_parsis_changed(vec3):
 			pl_data.nearstar_p_orb_ecc,
 			pl_data.nearstar_p_rtperiod,
 			pl_data.nearstar_p_rotation,
+			pl_data.nearstar_p_viewpoint,
 			pl_data.nearstar_p_term_start,
 			pl_data.nearstar_p_term_end,
 			pl_data.nearstar_p_qsortindex,
