2476b4d9313cc4cf626d672aab886be08663d938
joris <jorisvddonk@gmail.com>
2023-08-04 13:55:26 +0200

    show FCS status

--- hud.gd
+++ hud.gd
@@ -7,18 +7,27 @@ func _ready():
 	Globals.on_ap_target_changed.connect(_on_ap_target_changed)
 	Globals.vimana_status_change.connect(_on_vimana_status_changed)
 	_on_parsis_changed(Globals.feltyrion.ap_target_x, Globals.feltyrion.ap_target_y, Globals.feltyrion.ap_target_z)
+	var timer = Timer.new()
+	timer.timeout.connect(update_fcs_status)
+	timer.wait_time = 1
+	timer.one_shot = false
+	add_child(timer)
+	timer.start()
 
 func _on_parsis_changed(x, y, z):
-	$HBoxContainer/ParsisLabel.text = "[center]Parsis: x=%s y=%s z=%s[/center]" % [int(x), int(-y), int(z)]
+	$VBoxContainer/HBoxContainer/ParsisLabel.text = "[center]Parsis: x=%s y=%s z=%s[/center]" % [int(x), int(-y), int(z)]
 	
 func _on_vimana_status_changed(vimana_is_active):
 	if vimana_is_active:
-		$HBoxContainer/NumBodies.hide()
+		$VBoxContainer/HBoxContainer/NumBodies.hide()
 	else:
 		var nearstar_info = Globals.feltyrion.get_current_star_info()
-		$HBoxContainer/NumBodies.show()
-		$HBoxContainer/NumBodies.text = "[center]Number of bodies: %s[/center]" % [nearstar_info.nearstar_nob]
+		$VBoxContainer/HBoxContainer/NumBodies.show()
+		$VBoxContainer/HBoxContainer/NumBodies.text = "[center]Number of bodies: %s[/center]" % [nearstar_info.nearstar_nob]
 
 func _on_ap_target_changed(x, y, z, id_code):
 	ap_target_info = Globals.feltyrion.get_ap_target_info()
-	$HBoxContainer/APTarget.text = "[center]remote target: x=%s y=%s z=%s [/center]" % [x, -y, z]
+	$VBoxContainer/HBoxContainer/APTarget.text = "[center]remote target: x=%s y=%s z=%s [/center]" % [x, -y, z]
+
+func update_fcs_status():
+	$VBoxContainer/FCSStatus.text = "[right]%s[/right]" % Globals.feltyrion.get_fcs_status()
