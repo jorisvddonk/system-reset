b82110c98a457822a2b83619919457c494a60950
joris <jorisvddonk@gmail.com>
2023-08-08 18:48:09 +0200

    show remote/local/near as the top layer, depending on which UI mode is selected

--- main.gd
+++ main.gd
@@ -3,6 +3,7 @@ extends Control
 func _ready():
 	get_viewport().connect("size_changed", _on_resize)
 	set_process_unhandled_input(true)
+	Globals.ui_mode_changed.connect(ui_mode_changed)
 
 func _on_resize():
 	printt("Root viewport size changed", get_viewport().size)
@@ -12,9 +13,25 @@ func _on_resize():
 	$SubViewportContainer_SpaceLocal/SubViewport.size.y = get_viewport().size.y
 	$SubViewportContainer_SpaceNear/SubViewport.size.x = get_viewport().size.x
 	$SubViewportContainer_SpaceNear/SubViewport.size.y = get_viewport().size.y
+	$Background.size.x = get_viewport().size.x
+	$Background.size.y = get_viewport().size.y
 
 func _unhandled_input(event):
 	var evt1 = event.duplicate()
 	var evt2 = event.duplicate()
 	$SubViewportContainer_SpaceRemote/SubViewport.push_input(evt1, false)
 	$SubViewportContainer_SpaceNear/SubViewport.push_input(evt2, false)
+
+func ui_mode_changed(ui_mode):
+	if ui_mode == Globals.UI_MODE.SET_REMOTE_TARGET:
+		$SubViewportContainer_SpaceLocal.z_index = 1
+		$SubViewportContainer_SpaceNear.z_index = 2
+		$SubViewportContainer_SpaceRemote.z_index = 3
+	elif ui_mode == Globals.UI_MODE.SET_LOCAL_TARGET:
+		$SubViewportContainer_SpaceRemote.z_index = 1
+		$SubViewportContainer_SpaceNear.z_index = 2
+		$SubViewportContainer_SpaceLocal.z_index = 3
+	elif ui_mode == Globals.UI_MODE.NONE:
+		$SubViewportContainer_SpaceRemote.z_index = 0
+		$SubViewportContainer_SpaceLocal.z_index = 1
+		$SubViewportContainer_SpaceNear.z_index = 2
