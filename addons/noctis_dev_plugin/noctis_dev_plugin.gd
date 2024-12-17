6740ba3aa818afcce4b3f57dc4d7f8cd2ba363fa
Joris van de Donk <jorisvddonk@gmail.com>
2024-12-17 21:58:10 +0100

    empty out noctis-dev-plugin functionality; it wasn't doing anything

--- addons/noctis_dev_plugin/noctis_dev_plugin.gd
+++ addons/noctis_dev_plugin/noctis_dev_plugin.gd
@@ -2,11 +2,10 @@
 extends EditorPlugin
 
 func _enter_tree():
-	add_tool_menu_item("Simulate Vimana Flight", callback)
+	pass
 
 func callback():
 	pass
-	#Globals.vimanaStart()
 
 func _exit_tree():
-	remove_tool_menu_item("Simulate Vimana Flight")
+	pass
