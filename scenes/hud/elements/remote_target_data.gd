faa0a969b293974f451b726242dfef8086bab695
Joris van de Donk <jorisvddonk@gmail.com>
2024-12-31 13:25:07 +0100

    move get_starmass to feltyrion-godot; fixes #48

--- scenes/hud/elements/remote_target_data.gd
+++ scenes/hud/elements/remote_target_data.gd
@@ -6,33 +6,6 @@ func _ready():
 	visibility_changed.connect(_visibility_changed)
 	Globals.tick.connect(refresh)
 	Globals.on_ap_target_changed.connect(_ap_target_changed)
-	
-func get_starmass_correction(starclass):
-	# TODO: move this to feltyrion-godot
-	if starclass == 0:
-		return 1.886
-	if starclass == 1:
-		return 1.50   
-	if starclass == 2:
-		return 8000.40
-	if starclass == 3:
-		return 0.05   
-	if starclass == 4:
-		return 2.44   
-	if starclass == 5:
-		return 3.10   
-	if starclass == 6:
-		return 9.30   
-	if starclass == 7:
-		return 48.00  
-	if starclass == 8:
-		return 1.00 # TODO: this has some additional randomness correction!
-	if starclass == 9:
-		return 1.00 # TODO: this has some additional randomness correction!
-	if starclass == 10:
-		return 0.07   
-	if starclass == 11:
-		return 15000.00
 		
 func refresh():
 	if Globals.feltyrion.ap_targetted == 1:
@@ -41,7 +14,7 @@ func refresh():
 		var targetInfo = Globals.feltyrion.get_ap_target_info()
 		%HeaderLabel.text = "%s" % starName # starName already contains class info
 		var radius = targetInfo.ap_target_ray
-		var mass = 0.001 * (4 * PI / 3) * radius * radius * radius * get_starmass_correction(targetInfo.ap_target_class)
+		var mass = Globals.feltyrion.get_ap_starmass()
 		var temperature = mass / (0.38e-4 * targetInfo.ap_target_ray)
 		var starnop = Globals.feltyrion.get_starnop_estimate(Globals.feltyrion.ap_target_x, Globals.feltyrion.ap_target_y, Globals.feltyrion.ap_target_z)
 		if targetInfo.ap_target_class == 6:
