cc8e93d74d841878e5ae00ac326ed05db72430b6
Joris van de Donk <jorisvddonk@gmail.com>
2025-01-03 20:40:15 +0100

    fix: don't show data UIs when on the surface

--- scenes/hud/hud.gd
+++ scenes/hud/hud.gd
@@ -13,6 +13,7 @@ func _ready():
 	Globals.update_hud_selected_planet_text_request.connect(update_planetlabel_text)
 	Globals.update_hud_lightyears_text_request.connect(update_lightyears_text)
 	Globals.update_hud_dyams_text_request.connect(update_dyams_text)
+	Globals.gameplay_mode_changed.connect(on_gameplay_mode_changed)
 	Globals.data_ui_mode_changed.connect(on_data_ui_mode_changed)
 	Globals.tick.connect(_tick)
 	
@@ -187,12 +188,24 @@ func _tick():
 	else:
 		%SystemReset.show()
 	
-func on_data_ui_mode_changed(data_ui_mode):
-	if data_ui_mode == Globals.DATA_UI_MODE.NONE:
+func on_data_ui_mode_changed(_data_ui_mode):
+	updateDataUIVisibility()
+
+func hideAllDataUIs():
+	%RemoteTargetData.hide()
+	
+func updateDataUIVisibility():
+	if Globals.data_ui_mode == Globals.DATA_UI_MODE.NONE:
 		%RemoteTargetData.hide()
-	elif data_ui_mode == Globals.DATA_UI_MODE.REMOTE_TARGET:
+	elif Globals.data_ui_mode == Globals.DATA_UI_MODE.REMOTE_TARGET:
 		%RemoteTargetData.show()
-	elif data_ui_mode == Globals.DATA_UI_MODE.LOCAL_TARGET:
+	elif Globals.data_ui_mode == Globals.DATA_UI_MODE.LOCAL_TARGET:
 		%RemoteTargetData.hide()
-	elif data_ui_mode == Globals.DATA_UI_MODE.ENVIRONMENT:
+	elif Globals.data_ui_mode == Globals.DATA_UI_MODE.ENVIRONMENT:
 		%RemoteTargetData.hide()
+
+func on_gameplay_mode_changed(new_gameplay_mode):
+	if new_gameplay_mode == Globals.GAMEPLAY_MODE.SURFACE:
+		hideAllDataUIs()
+	elif new_gameplay_mode == Globals.GAMEPLAY_MODE.SPACE:
+		updateDataUIVisibility()
