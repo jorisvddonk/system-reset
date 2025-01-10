6b0342ebe8f1ccb561b682afd8bc80ab1f560994
Joris van de Donk <jorisvddonk@gmail.com>
2025-01-10 23:43:15 +0100

    implement bird fly/land behaviour

--- scenes/surface/animals/bird.gd
+++ scenes/surface/animals/bird.gd
@@ -5,34 +5,41 @@ var speed: float = 0.0
 const rayCheckVectorBegin = Vector3.UP * 10
 const rayCheckVectorEnd = Vector3.DOWN * 1000
 
-var step_tween: Tween
+var speed_tween: Tween
 var rotate_tween: Tween
+var height_tween: Tween
 
 var timeToNextAction: float = 0.0
+
+var flying = false
+var heightAboveSurface: float = 0.0
 
 
 func _ready():
 	rotate_y(randf_range(0, PI * 2))
 
-
 func _process(delta):
 	var velocity = -Basis().from_euler(rotation).z.normalized() * speed
-	position = position + velocity * delta
+	position = position + (velocity * delta)
 	var collision = _getCollisionPoint()
 	if collision:
 		if collision.position:			
-			position = collision.position
+			position = collision.position + (Vector3.UP * heightAboveSurface)
 	timeToNextAction -= delta
 	if timeToNextAction < 0:
 		timeToNextAction = 1.5 + randf_range(0, 1)
-		var r = randi() % 4
-		if r <= 1:
-			speed = 1.0
-			_step()
-		elif r == 2:
+		var f = randi() % 2
+		if f == 0:
+			if not flying:
+				_fly()
+		elif f == 1:
+			if flying:
+				_land()
+		elif f == 3:
 			_rotate()
-		elif r == 3:
-			pass # do nothing
+		var r = randi() % 2
+		if r == 0:
+			_rotate()
 
 
 func _getCollisionPoint():
@@ -47,14 +54,38 @@ func _getCollisionPoint():
 	var result = space_state.intersect_ray(query)
 	return result
 
-func _step():
-	if step_tween:
-		step_tween.kill()
-	step_tween = get_tree().create_tween()
-	step_tween.tween_property(self, "speed", 0, 1).set_trans(Tween.TRANS_SINE)
-
 func _rotate():
 	if rotate_tween:
 		rotate_tween.kill()
 	rotate_tween = get_tree().create_tween()
 	rotate_tween.tween_property(self, "quaternion", Quaternion((Vector3.UP*0.5).normalized(), randf_range((-3*PI)/4, (3*PI)/4)), 1).as_relative().set_trans(Tween.TRANS_SINE)
+
+func _fly():
+	flying = true
+	$birdy/AnimationTree["parameters/conditions/flying"] = true
+	$birdy/AnimationTree["parameters/conditions/landed"] = false
+	
+	if height_tween:
+		height_tween.kill()
+	height_tween = get_tree().create_tween()
+	height_tween.tween_property(self, "heightAboveSurface", 15  + randf_range(0, 25), 3).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	
+	if speed_tween:
+		speed_tween.kill()
+	speed_tween = get_tree().create_tween()
+	speed_tween.tween_property(self, "speed", 6 + randf_range(0, 20), 1.5).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN)
+	
+func _land():
+	flying = false
+	$birdy/AnimationTree["parameters/conditions/flying"] = false
+	$birdy/AnimationTree["parameters/conditions/landed"] = true
+	
+	if height_tween:
+		height_tween.kill()
+	height_tween = get_tree().create_tween()
+	height_tween.tween_property(self, "heightAboveSurface", 0, 1.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	
+	if speed_tween:
+		speed_tween.kill()
+	speed_tween = get_tree().create_tween()
+	speed_tween.tween_property(self, "speed", 0, 1.5).set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_IN)
